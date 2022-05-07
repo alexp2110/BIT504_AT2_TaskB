d9005793e7dfcae9d81d19b70f4a68f7e7e2c266
alexp2110 <platt.a@outlook.com>
2022-05-08 11:57:26 +1200

    Update file with complete Pong game, initial upload was incomplete file.

--- Pong/src/PongPanel.java
+++ Pong/src/PongPanel.java
@@ -9,11 +9,25 @@ import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.Stroke;
 import java.awt.BasicStroke;
+import java.awt.Font;
 
 public class PongPanel extends JPanel implements ActionListener, KeyListener {
 	
 	private final static Color backgroundColour = Color.BLACK;
 	private final static int timerDelay = 5;
+	private final int ballMovementSpeed = 6;
+	private final int paddleMovementSpeed = 5;
+	private final static int pointsToWin = 3;
+	private final static int scoreFontXPadding = 100;
+	private final static int scoreFontYPadding = 100;
+	private final static int scoreFontSize = 50;
+	private final static int winFontXPadding = 200;
+	private final static int winFontYPadding = 300;
+	private final static int winFontSize = 60;
+	private final static String font = "Serif";
+	private final static String winMessage = "WIN!";
+	int player1Score = 0, player2Score = 0;
+	Player gameWinner;
 	Ball ball;
 	GameState gameState = GameState.Initialising;
 	Paddle paddle1, paddle2;
@@ -22,6 +36,8 @@ public class PongPanel extends JPanel implements ActionListener, KeyListener {
 		setBackground(backgroundColour);
 		Timer timer = new Timer(timerDelay, this);
 			timer.start();
+			addKeyListener(this);
+			setFocusable(true);
 	}
 		
 	@Override
@@ -32,13 +48,28 @@ public class PongPanel extends JPanel implements ActionListener, KeyListener {
 
 	@Override
 	public void keyPressed(KeyEvent event) {
-		// TODO Auto-generated method stub
+		if(event.getKeyCode() == KeyEvent.VK_UP) {
+            paddle2.setYVelocity(-paddleMovementSpeed);
+		} else if(event.getKeyCode() == KeyEvent.VK_DOWN) {
+            paddle2.setYVelocity(paddleMovementSpeed);
+        }
 		
+		if(event.getKeyCode() == KeyEvent.VK_W) {
+            paddle1.setYVelocity(-paddleMovementSpeed);
+		} else if(event.getKeyCode() == KeyEvent.VK_S) {
+            paddle1.setYVelocity(paddleMovementSpeed);
+        }
 	}
 
 	@Override
 	public void keyReleased(KeyEvent event) {
-		// TODO Auto-generated method stub
+		if(event.getKeyCode() == KeyEvent.VK_UP || event.getKeyCode() == KeyEvent.VK_DOWN) {
+            paddle2.setYVelocity(0);
+        }
+		
+		if(event.getKeyCode() == KeyEvent.VK_W || event.getKeyCode() == KeyEvent.VK_S) {
+            paddle1.setYVelocity(0);
+        }
 		
 	}
 
@@ -47,9 +78,17 @@ public class PongPanel extends JPanel implements ActionListener, KeyListener {
 		case Initialising: {
 			createObjects();
 			gameState = GameState.Playing;
+			ball.setXVelocity(ballMovementSpeed);
+			ball.setYVelocity(ballMovementSpeed);
 			break;
 		}
 		case Playing: {
+			moveObject(paddle1); // move paddle 1
+			moveObject(paddle2); // move paddle 2
+			moveObject(ball);	// move ball
+			checkWallBounce();	// check for wall bounce
+			checkPaddleBounce(); // check for paddle bounce
+			checkWin();
 			break;
 		}
 		case GameOver: {
@@ -72,6 +111,8 @@ public class PongPanel extends JPanel implements ActionListener, KeyListener {
 			paintSprite(g, ball);
 			paintSprite(g, paddle1);
 			paintSprite(g, paddle2);
+			paintScores(g);
+			paintWin(g, gameWinner);
 		}
 	}
 	
@@ -94,5 +135,91 @@ public class PongPanel extends JPanel implements ActionListener, KeyListener {
 		g.setColor(sprite.getColour());
 		g.fillRect(sprite.getXPosition(), sprite.getYPosition(), sprite.getWidth(), sprite.getHeight());
 	}
-
+	
+	private void moveObject(Sprite obj) {
+		obj.setXPosition(obj.getXPosition() + obj.getXVelocity(), getWidth());
+		obj.setYPosition(obj.getYPosition() + obj.getYVelocity(), getHeight());
+	}
+	
+	private void checkWallBounce() {
+		int xVelocity, yVelocity;
+		
+		if (ball.getXPosition() <= 0) { // Ball has hit the left side of the screen.
+			xVelocity = ball.getXVelocity() * -1;
+			addScore(Player.Two);
+			resetBall();
+		}
+		else if (ball.getXPosition() >= getWidth() - ball.getWidth()) { // Ball has hit the right side of the screen.
+			xVelocity = ball.getXVelocity() * -1;
+			addScore(Player.One);
+			resetBall();
+		}
+		else {
+			xVelocity = ball.getXVelocity();
+		}
+		
+		if (ball.getYPosition() <= 0 || ball.getYPosition() >= getHeight() - ball.getHeight()) { // Ball has hit the top of the screen.
+			yVelocity = ball.getYVelocity() * -1;
+		}
+		else {
+			yVelocity = ball.getYVelocity();
+		}
+		ball.setXVelocity(xVelocity);
+		ball.setYVelocity(yVelocity);
+	}
+	
+	private void resetBall() {
+		ball.resetToInitialPosition();
+	}
+	
+	private void checkPaddleBounce() {
+		if(ball.getXVelocity() < 0 && ball.getRectangle().intersects(paddle1.getRectangle())) {
+			ball.setXVelocity(ballMovementSpeed);
+		}
+		if(ball.getXVelocity() > 0 && ball.getRectangle().intersects(paddle2.getRectangle())) {
+			ball.setXVelocity(-ballMovementSpeed);
+		}
+	}
+	
+	private void addScore(Player player) {
+		if(player == Player.One) {
+			player1Score++;
+		}
+		if(player == Player.Two) {
+			player2Score++;
+		}
+	}
+	
+	private void checkWin() {
+		if(player1Score >= pointsToWin) {
+			gameWinner = Player.One;
+			gameState = GameState.GameOver;
+		}
+		else if (player2Score >= pointsToWin) {
+			gameWinner = Player.Two;
+			gameState = GameState.GameOver;
+		}
+	}
+	
+	private void paintScores(Graphics g) {
+		Font scoreFont = new Font(font, Font.BOLD, scoreFontSize);
+		String leftScore = Integer.toString(player1Score);
+		String rightScore = Integer.toString(player2Score);
+		int rightScoreWidth = g.getFontMetrics().stringWidth(rightScore);
+		g.setFont(scoreFont);
+		g.drawString(leftScore, scoreFontXPadding, scoreFontYPadding);
+		g.drawString(rightScore, getWidth() - scoreFontXPadding - rightScoreWidth, scoreFontYPadding);
+	}
+	
+	private void paintWin(Graphics g, Player player) {
+		Font winFont = new Font(font, Font.BOLD, winFontSize);
+		int winMessageWidth = g.getFontMetrics().stringWidth(winMessage);
+		g.setFont(winFont);
+		if(player == Player.One) {
+			g.drawString(winMessage, getWidth() - winFontXPadding - winMessageWidth, winFontYPadding);
+		}
+		if(player == Player.Two) {
+			g.drawString(winMessage, winFontXPadding, winFontYPadding);
+		}
+	}
 }
